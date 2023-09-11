2cce82d65a316d75a2207aaaebb8e13e843fe624
kongwykon <weykonkong@gmail.com>
2023-09-11 09:46:52 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use winit::{window::Window, event::WindowEvent};
+
+struct State {
+    // 表面理解一下
+    // 平面， 设备信息，程序队列，配置，屏幕大小的信息
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    // 创建某些 wgpu 类型需要使用异步代码
+    async fn new(window: &Window) -> Self {
+        todo!()
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
